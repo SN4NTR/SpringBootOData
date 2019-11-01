b320f2e47df885807f6dea57c2e55156e66ba70f
Aliaksandr Miron <aliaksandr.miron@LeverX.local>
2019-11-01 17:54:42 +0300

    added configuration for cloud

---
+++ src/main/java/com/company/springbootodata/config/CloudDatabaseConfig.java
@@ -0,0 +1,29 @@
+package com.company.springbootodata.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("cf")
+public class CloudDatabaseConfig extends AbstractCloudConfig {
+
+    @Bean
+    public DataSource dataSource(@Value("${hana.url}") final String url,
+                                 @Value("${hana.user}") final String user,
+                                 @Value("${hana.password}") final String password) {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .driverClassName(com.sap.hana.cloud.hcp.cf.service.relational.HANADataSourceCreator.class.getName())
+                .url(url)
+                .username(user)
+                .password(password)
+                .build();
+    }
+}
