2ffb8068e329fe482ab183de67aa68f69c48d83d
Aliaksandr Miron <aliaksandr.miron@LeverX.local>
2019-11-05 12:05:31 +0300

    added port test

--- src/test/java/com/company/springbootodata/SpringBootODataApplicationTests.java
+++ src/test/java/com/company/springbootodata/SpringBootODataApplicationTests.java
@@ -2,11 +2,20 @@ package com.company.springbootodata;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 class SpringBootODataApplicationTests {
 
+    @LocalServerPort
+    private int actualServerPort;
+
     @Test
-    void contextLoads() {
+    void portChecking() {
+        int expectedServerPort = 8080;
+
+        assertEquals(expectedServerPort, actualServerPort);
     }
 }
